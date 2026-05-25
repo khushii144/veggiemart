82b1b496a04f82e9f34a6aec6986e8217d4e174b
khushii144 <khushi2026y@gmail.com>
2026-05-25 16:04:18 +0530

    Fix typo in eslint-disable comment in Navbar.js

--- frontend/src/components/shared/Navbar.js
+++ frontend/src/components/shared/Navbar.js
@@ -1,5 +1,5 @@
 "use client";
-/* eslint-disable react-hooks/exhaustive-depsghgf */
+/* eslint-disable react-hooks/exhaustive-depsghgfg */
 import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect, useMemo, useState } from 'react';
